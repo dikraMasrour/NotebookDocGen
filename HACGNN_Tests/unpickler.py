d60ad4c4cfe616b4065df8192ad9d7c695538b6a
dikramasrour <dirkra.masrour@uir.ac.ma>
2022-08-17 12:58:49 +0100

    Unpickle HAConvGNN data

--- HACGNN_Tests/unpickler.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import pandas as pd
-
-from tokenizer import Tokenizer
-
-with open('C:\\Users\\dmasrour\\Documents\\CodeDoc_Generation\\Data\\data_notebookcdg.pkl', 'rb') as f:
-    data = pickle.load(f)
-
-
