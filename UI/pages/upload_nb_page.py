66a20deea471b8c244f459067f2542a9c6f4e8ee
Dikra <dirkra.masrour@uir.ac.ma>
2022-09-29 01:15:03 +0100

    make main page and uploading page demo

---
+++ UI/pages/upload_nb_page.py
@@ -0,0 +1,26 @@
+from opcode import stack_effect
+import streamlit as st
+from streamlit_ace import st_ace
+
+st.set_page_config(
+    page_title="Magic notebook",
+    page_icon="✨",
+)
+
+
+st.title("Generated documentation for your DS & ML notebooks")
+st.header("")
+
+
+# with st.expander(" About the app"):
+
+#     st.write(
+#         """     
+
+# The *Generated documentation for your DS & ML notebooks* app is an easy-to-use interface where you can uploade your notebook containning only code cells and get in return a commented notebook !
+
+# 	    """
+#     )
+
+
+
